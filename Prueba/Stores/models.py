e76c68c74c209cdb106741867c552b34ba71b409
Jesus Cueva <jesuscueva1203@outlook.com>
2023-02-27 04:18:02 -0600

    Successfull deployment of MariaDB

---
+++ Prueba/Stores/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Store(models.Model):
+    id = models.IntegerField(primary_key=True)
+    brand = models.IntegerField()
+    identifier = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
